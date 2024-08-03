05f03a40742971fbf24c018ba05a253e495ff72d
Tong <zhangt766@gmail.com>
2024-08-04 08:41:53 +1000

    Update train_lastfm.sh

--- train_lastfm.sh
+++ train_lastfm.sh
@@ -4,26 +4,6 @@ SLEEP_SECONDS=3600
 # 睡眠1小时
 sleep $SLEEP_SECONDS
 
-CUDA_VISIBLE_DEVICES=2 python3 main.py \
---mode train \
---batch_size 8 \
---accumulate_grad_batches 8 \
---dataset movielens_data \
---data_dir data/ref/lastfm \
---cans_num 20 \
---prompt_path ./prompt/artist.txt \
---rec_embed SASRec \
---llm_tuning lora \
---llm_path /mnt/bn/data-tns-live-llm/leon/datasets/rec/score_model \
---rec_model_path ./rec_model/lastfm.pt \
---output_dir /mnt/bn/data-tns-live-llm/leon/datasets/rec/lastfm \
---log_dir lastfm_logs \
---lr_warmup_start_lr 2e-6 \
---lr 2e-4 \
---lr_decay_min_lr 2e-6 \
---max_epochs 5 \
---precision bf16 \
---unsloth 0
 
 CUDA_VISIBLE_DEVICES=1 python3 main.py \
 --mode train \
@@ -37,7 +17,7 @@ CUDA_VISIBLE_DEVICES=1 python3 main.py \
 --llm_tuning lora \
 --llm_path /mnt/bn/data-tns-live-llm/leon/datasets/llama-2-7b-bnb-4bit  \
 --rec_model_path ./rec_model/lastfm.pt \
---output_dir /mnt/bn/data-tns-live-llm/leon/datasets/rec/lastfm_unsloth \
+--output_dir output/lastfm_unsloth \
 --log_dir lastfm_logs \
 --lr_warmup_start_lr 2e-6 \
 --lr 2e-4 \
