ebe3948ece2a56a8768a7d09b75828eed74f09d7
Tong <zhangt766@gmail.com>
2024-08-04 09:06:30 +1000

    Add files via upload

---
+++ test_lastfm.sh
@@ -0,0 +1,39 @@
+CUDA_VISIBLE_DEVICES=2 python3 main.py \
+--mode test \
+--batch_size 4 \
+--accumulate_grad_batches 32 \
+--dataset movielens_data \
+--data_dir data/ref/lastfm \
+--cans_num 20 \
+--prompt_path ./prompt/artist.txt \
+--rec_embed SASRec \
+--llm_tuning lora \
+--llm_path /mnt/bn/data-tns-live-llm/leon/datasets/rec/movielens_unsloth \
+--rec_model_path ./rec_model/lastfm.pt \
+--output_dir /output/lastfm/ \
+--log_dir lastfm_logs \
+--lr_warmup_start_lr 2e-6 \
+--lr 2e-4 \
+--lr_decay_min_lr 2e-6 \
+--max_epochs 5 \
+--unsloth 1
+
+CUDA_VISIBLE_DEVICES=2 python3 main.py \
+--mode test \
+--batch_size 4 \
+--accumulate_grad_batches 32 \
+--dataset movielens_data \
+--data_dir data/ref/lastfm \
+--cans_num 20 \
+--prompt_path ./prompt/artist.txt \
+--rec_embed SASRec \
+--llm_tuning lora \
+--llm_path LEON24/recommend \
+--rec_model_path ./rec_model/lastfm.pt \
+--output_dir /output/lastfm/ \
+--log_dir lastfm_logs \
+--lr_warmup_start_lr 2e-6 \
+--lr 2e-4 \
+--lr_decay_min_lr 2e-6 \
+--max_epochs 5 \
+--unsloth 1
