f19a56c60c2c23746fee84cc7a3cc64390d4c3f2
Tong <zhangt766@gmail.com>
2025-04-10 11:03:33 +1000

    Update train_lastfm.sh

--- train_lastfm.sh
+++ train_lastfm.sh
@@ -2,19 +2,16 @@ CUDA_VISIBLE_DEVICES=0 python3 main.py \
 --mode train \
 --batch_size 8 \
 --accumulate_grad_batches 8 \
---dataset movielens_data \
+--dataset lastfm \
 --data_dir data/ref/lastfm \
 --cans_num 20 \
---prompt_path ./prompt/artist.txt \
---rec_embed SASRec \
 --llm_tuning lora \
---llm_path /workspace/Llama-2-7b-hf  \
---rec_model_path ./rec_model/lastfm.pt \
---output_dir output/lastfm_unsloth \
---log_dir lastfm_logs \
+--llm_path /workspace/Llama-2-7b-hf \
+--output_dir output/adaptrec_lastfm \
+--log_dir adaptrec_lastfm_logs \
 --lr_warmup_start_lr 2e-6 \
 --lr 2e-4 \
 --lr_decay_min_lr 2e-6 \
 --max_epochs 5 \
 --precision bf16 \
---unsloth 1 
+--unsloth 1
